03db595c36aad360d4ed526a2d39edbed062667c
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-04-27 21:51:06 +0530

    initialized code page

---
+++ app/code/page.tsx
@@ -0,0 +1,3 @@
+export default function CodePage() {
+  return <main>editor</main>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <section className="grid h-96 content-center">
         <div>
           <Link
-            href={"/"}
+            href={"/code"}
             className="mx-auto block w-max rounded-md border border-transparent bg-white/[.1] px-4 py-3 text-sm font-semibold text-gray-100 ring-offset-white transition-all hover:bg-white hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 dark:focus:ring-offset-gray-800"
           >
             Create new +
