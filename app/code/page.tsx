b5290c92d5e0c91d0191519dff16e2185e98a26a
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-04-29 12:14:11 +0530

    added monaco and matirial file icons

--- app/code/page.tsx
+++ app/code/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import Editor from "@monaco-editor/react";
+
 export default function CodePage() {
-  return <main>editor</main>;
+  return (
+    <div>
+      <Editor
+        height="90vh"
+        defaultLanguage="javascript"
+        theme="vs-dark"
+        defaultValue={"span"}
+      />
+    </div>
+  );
 }
