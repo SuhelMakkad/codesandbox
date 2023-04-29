f0ce262d55844ddf1b06333c6518e64722f8f971
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-04-29 15:53:03 +0530

    added tob bart

--- app/code/page.tsx
+++ app/code/page.tsx
@@ -4,20 +4,23 @@ import Editor from "@monaco-editor/react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 import Explorer from "@/standalone/Explorer";
+import FileTabs from "@/standalone/FileTabs";
 
 export default function CodePage() {
   return (
     <PanelGroup direction="horizontal">
-      <Panel>
+      <Panel minSize={0} defaultSize={20}>
         <Explorer />
       </Panel>
 
-      <PanelResizeHandle className="w-1 bg-neutral-400 hover:bg-blue-300 data-[resize-handle-active]:bg-blue-300" />
+      <PanelResizeHandle className="w-[0.2rem] bg-neutral-800 transition-[background-color] hover:bg-blue-300 data-[resize-handle-active]:bg-blue-300" />
 
-      <Panel>
+      <Panel minSize={50}>
+        <FileTabs className="h-[2rem]" />
+
         <Editor
           className="h-screen"
-          height={"100vh"}
+          height={"calc(100vh - 2rem)"}
           defaultLanguage="javascript"
           theme="vs-dark"
           defaultValue={"span"}
---
+++ common/components/standalone/FileTabs/index.tsx
@@ -0,0 +1,9 @@
+export type Props = {
+  className: string;
+};
+
+const FileTabs = ({ className }: Props) => {
+  return <div className={`${className}`}>FileTabs</div>;
+};
+
+export default FileTabs;
