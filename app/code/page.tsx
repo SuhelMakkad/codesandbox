9a8de199548720c29689e832dbb7ab22db25fcd5
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-04-29 15:44:05 +0530

    using react resiable panel

--- app/code/page.tsx
+++ app/code/page.tsx
@@ -1,23 +1,28 @@
 "use client";
 
 import Editor from "@monaco-editor/react";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 import Explorer from "@/standalone/Explorer";
-import Wrapper from "@/layout/ResizableTabs/Wrapper";
-import Tab from "@/layout/ResizableTabs/Tab";
 
 export default function CodePage() {
   return (
-    <Wrapper>
-      <Explorer />
+    <PanelGroup direction="horizontal">
+      <Panel>
+        <Explorer />
+      </Panel>
 
-      <Editor
-        className="h-screen"
-        height={"100vh"}
-        defaultLanguage="javascript"
-        theme="vs-dark"
-        defaultValue={"span"}
-      />
-    </Wrapper>
+      <PanelResizeHandle className="w-1 bg-neutral-400 hover:bg-blue-300 data-[resize-handle-active]:bg-blue-300" />
+
+      <Panel>
+        <Editor
+          className="h-screen"
+          height={"100vh"}
+          defaultLanguage="javascript"
+          theme="vs-dark"
+          defaultValue={"span"}
+        />
+      </Panel>
+    </PanelGroup>
   );
 }
