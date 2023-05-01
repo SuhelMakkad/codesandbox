8557f106ae5f9e240676c47a772d1dd439a9f074
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-05-01 17:13:22 +0530

    fixed issue with xterm

--- common/components/standalone/Xterm/index.tsx
+++ common/components/standalone/Xterm/index.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useRef } from "react";
+"use client";
 
-import { Terminal } from "xterm";
-import { FitAddon } from "xterm-addon-fit";
+import { useEffect, useRef } from "react";
 
 import "xterm/css/xterm.css";
 
@@ -10,89 +9,98 @@ const Xterm = () => {
 
   useEffect(() => {
     if (!divRef.current) return;
-    const socket = new WebSocket("ws://localhost:6060");
 
-    const term = new Terminal({
-      cursorBlink: true,
-    });
-    const fitAddon = new FitAddon();
-    let command = "";
+    const init = async () => {
+      if (!divRef.current) return;
 
-    term.loadAddon(fitAddon);
-    term.open(divRef.current);
+      const { Terminal } = await import("xterm");
+      const { FitAddon } = await import("xterm-addon-fit");
 
-    prompt(term);
+      const socket = new WebSocket("ws://localhost:6060");
+      const term = new Terminal({
+        cursorBlink: true,
+      });
+      const fitAddon = new FitAddon();
+      let command = "";
 
-    term.onData((e) => {
-      switch (e) {
-        case "\u0003": // Ctrl+C
-          term.write("^C");
-          prompt(term);
-          break;
-        case "\r": // Enter
-          runCommand(command);
-          command = "";
-          break;
-        case "\u007F": // Backspace (DEL)
-          // Do not delete the prompt
-          if ((term as any)._core.buffer.x > 2) {
-            term.write("\b \b");
-            if (command.length > 0) {
-              command = command.substr(0, command.length - 1);
+      term.loadAddon(fitAddon);
+      term.open(divRef.current);
+
+      prompt();
+
+      term.onData((e) => {
+        switch (e) {
+          case "\u0003": // Ctrl+C
+            term.write("^C");
+            prompt();
+            break;
+          case "\r": // Enter
+            runCommand(command);
+            command = "";
+            break;
+          case "\u007F": // Backspace (DEL)
+            // Do not delete the prompt
+            if ((term as any)._core.buffer.x > 2) {
+              term.write("\b \b");
+              if (command.length > 0) {
+                command = command.substr(0, command.length - 1);
+              }
             }
-          }
-          break;
-        case "\u0009":
-          console.log("tabbed", e, ["dd", "ls"]);
-          break;
-        default:
-          if (
-            (e >= String.fromCharCode(0x20) &&
-              e <= String.fromCharCode(0x7e)) ||
-            e >= "\u00a0"
-          ) {
-            command += e;
-            term.write(e);
-          }
+            break;
+          case "\u0009":
+            console.log("tabbed", e, ["dd", "ls"]);
+            break;
+          default:
+            if (
+              (e >= String.fromCharCode(0x20) &&
+                e <= String.fromCharCode(0x7e)) ||
+              e >= "\u00a0"
+            ) {
+              command += e;
+              term.write(e);
+            }
+        }
+      });
+
+      function clearInput(command: string) {
+        var inputLength = command.length;
+        for (var i = 0; i < inputLength; i++) {
+          term.write("\b \b");
+        }
       }
-    });
 
-    function clearInput(command: string) {
-      var inputLength = command.length;
-      for (var i = 0; i < inputLength; i++) {
-        term.write("\b \b");
+      function prompt() {
+        command = "";
+        term.write("\r\n$ ");
       }
-    }
 
-    function prompt(term: Terminal) {
-      command = "";
-      term.write("\r\n$ ");
-    }
+      socket.onmessage = (event) => {
+        term.write(event.data);
+      };
 
-    socket.onmessage = (event) => {
-      term.write(event.data);
+      function runCommand(command: string) {
+        if (command.length > 0) {
+          clearInput(command);
+          socket.send(command + "\n");
+          return;
+        }
+      }
+
+      function handleWrapperResize() {
+        console.log("called");
+        fitAddon.fit();
+      }
+
+      divRef.current.addEventListener("resize", handleWrapperResize);
     };
 
-    function runCommand(command: string) {
-      if (command.length > 0) {
-        clearInput(command);
-        socket.send(command + "\n");
-        return;
-      }
-    }
-
-    function handleWrapperResize() {
-      console.log("called");
-      fitAddon.fit();
-    }
-
-    divRef.current.addEventListener("resize", handleWrapperResize);
+    init();
 
     () => {
-      term.dispose();
-      fitAddon.dispose();
-      socket.close();
-      divRef.current?.removeEventListener("resize", handleWrapperResize);
+      // term?.dispose();
+      // fitAddon?.dispose();
+      // socket?.close();
+      // divRef.current?.removeEventListener("resize", handleWrapperResize);
     };
   }, [divRef]);
 
