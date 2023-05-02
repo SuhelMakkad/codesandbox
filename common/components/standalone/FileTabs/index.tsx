74f4c6901156ae4ba6f36594934d60d6fb0f9265
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-05-02 21:25:49 +0530

    changeing selected tab when clicked

--- common/components/standalone/FileTabs/index.tsx
+++ common/components/standalone/FileTabs/index.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import FileIcon from "@/ui/FileIcon";
+import { useState } from "react";
 
 export type Props = {
   files?: File[];
@@ -10,26 +13,32 @@ export type File = {
 };
 
 const FileTabs = ({ files }: Props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
   if (!files) return <></>;
 
   return (
-    <ol className="flex text-sm">
-      {files.map((file, index) => (
-        <li
-          key={file.name}
-          className={`border-r border-r-neutral-700 border-t-blue-400 ${
-            file.isActive
-              ? "border-t-2 text-white"
-              : "bg-neutral-800 text-neutral-400"
-          } ${index === files.length - 1 ? "border-r-0" : ""}`}
-        >
-          <button className="flex items-center gap-2 px-4 py-1">
-            <FileIcon filename={file.name} className="w-4 shrink-0" />
-            {file.name}
-          </button>
-        </li>
-      ))}
-    </ol>
+    <nav>
+      <ol className="flex text-sm">
+        {files.map((file, index) => (
+          <li
+            key={file.name}
+            className={`relative transition-[background-color,color,opacity] duration-100 after:absolute after:left-0 after:right-0 after:top-0 after:h-[2px] after:bg-blue-400 after:transition-opacity ${
+              index === activeIndex
+                ? "bg-transparent text-white after:opacity-100"
+                : "bg-neutral-800 text-neutral-400 after:opacity-0  hover:bg-neutral-700/25 hover:text-neutral-200"
+            } ${index === files.length - 1 ? "border-r-0" : ""}`}
+          >
+            <button
+              onClick={() => setActiveIndex(index)}
+              className="flex items-center gap-2 px-4 py-1.5"
+            >
+              <FileIcon filename={file.name} className="w-4 shrink-0" />
+              {file.name}
+            </button>
+          </li>
+        ))}
+      </ol>
+    </nav>
   );
 };
 
--- common/components/standalone/SideTabSelectorNa/index.tsx
+++ common/components/standalone/SideTabSelectorNa/index.tsx
@@ -23,7 +23,7 @@ const SideTabSelectorNav = () => {
         <button
           key={index}
           onClick={() => setActiveIndex(index)}
-          className={`relative px-2.5 py-3 transition-[color] after:absolute after:bottom-0 after:left-0 after:top-0 after:w-[2px] after:bg-white after:transition-opacity ${
+          className={`relative px-2.5 py-3 transition-[color] duration-100 after:absolute after:bottom-0 after:left-0 after:top-0 after:w-[2px] after:bg-white after:transition-opacity ${
             index === activeIndex
               ? "text-white after:opacity-100"
               : "after:opacity-0"
