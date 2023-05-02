cab791aac89b94c2fa08e00351dc96ec04d355ef
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-05-02 22:12:03 +0530

    added close button to file tabs

--- common/components/standalone/FileTabs/index.tsx
+++ common/components/standalone/FileTabs/index.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { MouseEvent, useState } from "react";
+
+import { VscChromeClose } from "react-icons/vsc";
+
 import FileIcon from "@/ui/FileIcon";
-import { useState } from "react";
 
 export type Props = {
   files?: File[];
@@ -14,6 +17,11 @@ export type File = {
 
 const FileTabs = ({ files }: Props) => {
   const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleClose = (e: MouseEvent, index: number) => {
+    e.stopPropagation();
+  };
+
   if (!files) return <></>;
 
   return (
@@ -22,14 +30,21 @@ const FileTabs = ({ files }: Props) => {
         <button
           key={file.name}
           onClick={() => setActiveIndex(index)}
-          className={` relative flex items-center gap-2 px-4 py-1.5 transition-[background-color,color,opacity] duration-100 after:absolute after:left-0 after:right-0 after:top-0 after:h-[2px] after:bg-blue-400 after:transition-opacity ${
+          className={`group relative flex items-center gap-2 py-2 pl-4 pr-3 transition-[background-color,color,opacity] duration-100 after:absolute after:left-0 after:right-0 after:top-0 after:h-[2px] after:bg-blue-400 after:transition-opacity ${
             index === activeIndex
               ? "bg-transparent text-white after:opacity-100"
               : "bg-neutral-800 text-neutral-400 after:opacity-0  hover:bg-neutral-700/25 hover:text-neutral-200"
-          } ${index === files.length - 1 ? "border-r-0" : ""}`}
+          }`}
         >
           <FileIcon filename={file.name} className="w-4 shrink-0" />
           {file.name}
+
+          <div
+            onClick={(e) => handleClose(e, index)}
+            className="rounded p-1 opacity-0 hover:bg-neutral-600 group-hover:opacity-100"
+          >
+            <VscChromeClose />
+          </div>
         </button>
       ))}
     </nav>
